22d30f824afa81164c5df681571434b2eb0b9c53
iko <mail@iko.soy>
2024-08-13 02:13:48 +0300

    .

--- flake.nix
+++ flake.nix
@@ -11,6 +11,10 @@
       url = "github:PeterScott/murmur3";
       flake = false;
     };
+    urcrypt = {
+      url = "github:urbit/urcrypt?rev=43479c3262a11e20da5f6218f3b0b3d63931ceea";
+      flake = false;
+    };
     pdjson = {
       url = "github:skeeto/pdjson";
       flake = false;
@@ -23,11 +27,19 @@
       url = "github:LekKit/sha256";
       flake = false;
     };
+    libaes_siv = {
+      url = "github:dfoxfranke/libaes_siv";
+      flake = false;
+    };
   };
   outputs =
     inputs@{ self, nixpkgs, flake-utils, vere, murmur3, pdjson, sha256, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = { permittedInsecurePackages = [ "openssl-1.1.1w" ]; };
+        };
       in {
         packages = rec {
           gmp = pkgs.runCommand "gmp" {
@@ -54,6 +66,68 @@
             '';
             doCheck = false;
           });
+          openssl = pkgs.openssl_1_1.overrideDerivation (old: {
+            configurePhase = ''
+              runHook preConfigure
+
+              HOME=$TMPDIR
+              export CC=${pkgs.emscripten}/bin/emcc
+              ./Configure linux-generic64 no-shared -no-asm no-engine no-tests --prefix=$out
+
+              runHook postConfigure
+            '';
+
+            postInstall = ''
+              mkdir -p $bin
+              mv $out/bin $bin/bin
+
+              mkdir $dev
+              mv $out/include $dev/
+            '';
+          });
+          libaes_siv = pkgs.runCommand "libaes_siv" {
+            buildInputs = with pkgs; [ emscripten ];
+          } ''
+            mkdir -p $out/lib
+            HOME=$TMPDIR
+            emcc ${inputs.libaes_siv}/aes_siv.c -I${
+              ./libaes_siv/include
+            } -I${openssl.dev}/include -o $out/lib/aes_siv.o
+            mkdir -p $out/include
+            cp ${inputs.libaes_siv}/aes_siv.h $out/include/aes_siv.h
+          '';
+          argon2 = pkgs.runCommand "argon2" {
+            buildInputs = with pkgs; [ emscripten ];
+          } ''
+            cp -r ${inputs.urcrypt}/argon2/src src
+            chmod -R +w src
+
+            mkdir -p $out/lib
+            HOME=$TMPDIR
+            emcc src/argon2.c src/blake2/blake2b.c src/core.c src/encoding.c src/thread.c -I${inputs.urcrypt}/argon2/include -o $out/lib/argon2.a
+
+            mkdir -p $out/include
+            cp ${inputs.urcrypt}/argon2/include/argon2.h src/blake2/blake2.h $out/include
+          '';
+          urcrypt = pkgs.runCommand "urcrypt" {
+            nativeBuildInputs = with pkgs; [ emscripten ];
+          } ''
+            cp -r ${inputs.urcrypt} src
+            chmod +w -R src
+
+            mkdir -p $out/lib
+            HOME=$TMPDIR
+            emcc -I${openssl.dev}/include -I${libaes_siv}/include \
+            src/argon2/src/argon2.c src/argon2/src/blake2/blake2b.c \
+            src/argon2/src/core.c src/argon2/src/encoding.c src/argon2/src/thread.c \
+            src/argon2/src/ref.c src/ed25519/src/*.c src/ge-additions/ge-additions.c \
+            src/keccak-tiny/keccak-tiny.c src/scrypt/*.c \
+            src/urcrypt/*.c \
+            -I${pkgs.secp256k1}/include -I${pkgs.libb2}/include -Isrc/argon2/include \
+            -Isrc/ed25519/src/ -Isrc/ge-additions -Isrc/keccak-tiny -Isrc/scrypt \
+            -Wno-int-conversion \
+            -o $out/lib/urcrypt.a
+          '';
           softfloat =
             pkgs.callPackage ./softfloat.nix { inherit (inputs) softfloat; };
           nock = pkgs.runCommand "nock" { } ''
@@ -71,7 +145,7 @@
               pkg/noun/*.c pkg/noun/jets/*/*.c pkg/noun/jets/*.c pkg/c3/*.c pkg/ur/*.c \
               pkg/noun/v3/*.c pkg/noun/v2/*.c pkg/noun/v1/*.c \
               ${murmur3}/murmur3.c ${pdjson}/pdjson.c ${gmp}/lib/libgmp.a ${sha256}/sha256.c \
-              ${softfloat}/lib/softfloat.a ${libsigsegv}/lib/libsigsegv.a \
+              ${softfloat}/lib/softfloat.a ${libsigsegv}/lib/libsigsegv.a ${urcrypt}/lib/urcrypt.a \
               -o $out/nock.js \
               -DU3_OS_linux \
               -DU3_OS_ENDIAN_little \
